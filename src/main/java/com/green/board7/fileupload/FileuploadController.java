28207b2ffaa9111637b9246917c4b5689388f53c
kdy <kdy12061004@gmail.com>
2023-05-25 16:22:20 +0900

    스앵님이 올려주신 html +

--- src/main/java/com/green/board7/fileupload/FileuploadController.java
+++ src/main/java/com/green/board7/fileupload/FileuploadController.java
@@ -1,14 +1,15 @@
 package com.green.board7.fileupload;
 
+import com.green.board7.fileupload.model.FileLoadDto;
 import com.green.board7.fileupload.model.FileuploadInsDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -21,6 +22,15 @@ public class FileuploadController {
     public FileuploadController(FileuploadService service) {
         LOGGER = LoggerFactory.getLogger(FileuploadController.class);
         this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<Resource> download(FileLoadDto dto) {
+        Resource file = service.fileLoad(dto);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=\"" + file.getFilename() + "\"")
+                .body(file);
     }
 
     @PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE
